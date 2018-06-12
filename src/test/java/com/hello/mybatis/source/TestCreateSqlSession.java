e0c8c39d1e42b45451b6e7818bf58564da8f9557
Administrator <Administrator@XB-201708182132>
2018-06-12 18:01:30 +0800

    mybaits

--- src/test/java/com/hello/mybatis/source/TestCreateSqlSession.java
+++ src/test/java/com/hello/mybatis/source/TestCreateSqlSession.java
@@ -1,5 +1,12 @@
 package com.hello.mybatis.source;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,7 +19,19 @@ public class TestCreateSqlSession extends BaseDAOTest{
 	private TestDAO dao;
 	
 	@Test
-	public void testCreateSqlSession(){
+	public void testQuery(){
 		System.out.println(dao.listPersons());
 	}
+	
+	public static void main(String[] args) throws IOException{
+		String config = "mybatis-config.xml";
+		Reader reader = Resources.getResourceAsReader(config);
+		SqlSessionFactoryBuilder sb = new SqlSessionFactoryBuilder();
+		//创建sqlSessionFactory的过程中解析mybatis-config配置文件，将xml文件中mapper解析
+		SqlSessionFactory sqlSessionFactory = sb.build(reader);
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		TestDAO testDAO = sqlSession.getMapper(TestDAO.class);
+		System.out.println(testDAO.listPersons());
+	}
+	
 }
