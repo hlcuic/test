1c0f76d17a9b27d6443aa4f91660808726e515bd
hello <hello@XB-201608181957>
2018-06-12 21:32:07 +0800

    test getSuperClass

--- src/test/java/com/hello/test/validate/TestValidateParamsByAnnotation.java
+++ src/test/java/com/hello/test/validate/TestValidateParamsByAnnotation.java
@@ -1,5 +1,7 @@
 package com.hello.test.validate;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 import com.hello.model.Person;
@@ -14,5 +16,29 @@ public class TestValidateParamsByAnnotation {
 		person.setName("tom");
 		ValidateUtils.doValidator(person);
 	}
+	
+	@Test
+	public void testGetSuperClass(){
+		Class<?> clazz = C.class;
+		//获取C类父类
+		System.out.println("C父类："+clazz.getSuperclass());
+		//获取C类父接口
+		System.out.println("C父接口:"+Arrays.toString(clazz.getInterfaces()));
+	}
+}
+
+class A{
+	
+}
+
+interface B extends D{
+	
+}
+
+interface D{
+	
+}
+
+class C extends A implements B{
 	
 }
