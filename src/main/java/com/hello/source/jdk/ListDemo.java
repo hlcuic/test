8fb0977ef321866a133ccd18528d43e1ebb8b129
Administrator <Administrator@XB-201708182132>
2018-06-19 17:48:56 +0800

    list demo

--- src/main/java/com/hello/source/jdk/ListDemo.java
+++ src/main/java/com/hello/source/jdk/ListDemo.java
@@ -2,6 +2,25 @@ package com.hello.source.jdk;
 
 public class ListDemo {
 	public static void main(String[] args) {
-		
+		testArrayList();
+		testLinkedList();
+		testHashSet();
+		testTreeSet();
+	}
+
+	private static void testArrayList() {
+
+	}
+
+	private static void testLinkedList() {
+
+	}
+
+	private static void testHashSet() {
+
+	}
+
+	private static void testTreeSet() {
+
 	}
 }
