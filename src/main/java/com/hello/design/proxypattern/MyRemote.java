db6b185b9b22cb9e4db919b37ccb8b3ac41f9deb
hello <hello@XB-201608181957>
2018-06-27 22:05:54 +0800

    proxy pattern

---
+++ src/main/java/com/hello/design/proxypattern/MyRemote.java
@@ -0,0 +1,8 @@
+package com.hello.design.proxypattern;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface MyRemote extends Remote{
+	String sayHello()throws RemoteException;
+}
---
+++ src/main/java/com/hello/design/proxypattern/MyRemoteClient.java
@@ -0,0 +1,22 @@
+package com.hello.design.proxypattern;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class MyRemoteClient {
+	public static void main(String[] args) {
+		new MyRemoteClient().go();
+	}
+	
+	public void go(){
+		try {
+			MyRemote service  = (MyRemote) Naming.lookup("rmi://127.0.0.1/RemoteHello");
+			String s = service.sayHello();
+			System.out.println(s);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
