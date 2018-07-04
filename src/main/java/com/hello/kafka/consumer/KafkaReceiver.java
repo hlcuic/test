808f331552b3c7fd614ce835c3ee4f416ba538f1
Administrator <Administrator@XB-201708182132>
2018-07-04 13:16:50 +0800

    kafka demo

--- src/main/java/com/hello/kafka/consumer/KafkaReceiver.java
+++ src/main/java/com/hello/kafka/consumer/KafkaReceiver.java
@@ -2,12 +2,14 @@ package com.hello.kafka.consumer;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 public class KafkaReceiver {
 
@@ -21,15 +23,15 @@ public class KafkaReceiver {
 
 	private Consumer<String, String> createKafkaConsumer() {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", "127.0.0.1:9092");
-		props.put("acks", "all");
-		props.put("retries", 0);
-		props.put("batch.size", 16384);
-		props.put("linger.ms", 1);
-		props.put("buffer.memory", 33554432);
-		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id", "1");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("max.poll.records", 1000);
+		props.put("auto.offset.reset", "earliest");
+		props.put("key.deserializer", StringDeserializer.class.getName());
+		props.put("value.deserializer", StringDeserializer.class.getName());
 		Consumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
 		return kafkaConsumer;
 	}
@@ -37,19 +39,15 @@ public class KafkaReceiver {
 	private void consumer() {
 		Collection<String> coll = new ArrayList<>();
 		coll.add(TOPIC);
-		kafkaConsumer.subscribe(coll, new ConsumerRebalanceListener(){
-
-			@Override
-			public void onPartitionsAssigned(Collection<TopicPartition> collection) {
-				
+		kafkaConsumer.subscribe(coll);
+		while (true) {
+			//1次从kafka服务器拉取1000条数据
+			ConsumerRecords<String, String> records = kafkaConsumer.poll(1000);
+			for (Iterator<ConsumerRecord<String, String>> iter = records.iterator(); iter.hasNext();) {
+				ConsumerRecord<String, String> record = iter.next();
+				System.out.println(record.key() + " , " + record.value());
 			}
-
-			@Override
-			public void onPartitionsRevoked(Collection<TopicPartition> collection) {
-				
-			}
-			
-		});
+		}
 	}
 
 	public static void main(String[] args) {
